c8236823b7dc93913d9dc0e1bf4fb7751942e11a
Kyla Palos <kyla.leatrice.palos@accenture.com>
2021-09-23 14:29:49 -0400

    SavingsCalculator module3

---
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -0,0 +1,69 @@
+package com.h2;
+
+import java.time.LocalDate;
+import java.time.YearMonth;
+
+
+public class SavingsCalculator {
+
+    private float[] credits;
+    private float[] debits;
+
+    public SavingsCalculator(float[] credits, float[] debits) {
+        this.credits = credits;
+        this.debits = debits;
+    }
+
+    private float sumOfCredits() {
+        var sum = 0.0f;
+        for (float credit : credits) {
+            sum += credit;
+        }
+        return sum;
+    }
+
+    private float sumOfDebits() {
+        var sum = 0.0f;
+        for (float debit : debits) {
+            sum += debit;
+        }
+
+        return sum;
+    }
+
+    private static int remainingDaysInMonth(LocalDate date) {
+        int year = date.getYear();
+        int month = date.getMonthValue();
+        YearMonth yearMonth = YearMonth.of(year, month);
+
+        int totalDaysInMonth = yearMonth.lengthOfMonth();
+
+        return totalDaysInMonth - date.getDayOfMonth();
+    }
+
+    public float calculate() {
+        return sumOfCredits() - sumOfDebits();
+    }
+
+    public static void main(String[] args) {
+        final String[] creditsAsString = args[0].split(",");
+        final String[] debitsAsString = args[1].split(",");
+
+        final float[] credits = new float[creditsAsString.length];
+        final float[] debits = new float[debitsAsString.length];
+
+        for (int i = 0; i < creditsAsString.length; i++) {
+            credits[i] = Float.parseFloat(creditsAsString[i]);
+        }
+
+        for (int i = 0; i < debitsAsString.length; i++) {
+            debits[i] = Float.parseFloat(debitsAsString[i]);
+        }
+
+        SavingsCalculator calculator = new SavingsCalculator(credits, debits);
+        float netSavings = calculator.calculate();
+        int remainingDays = remainingDaysInMonth(LocalDate.now());
+        System.out.println("Net Savings = " + netSavings + ", remaining days in month = " + remainingDays);
+    }
+
+}
